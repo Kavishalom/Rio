54cc0f080cc7c15e891192a2982ef82475087319
Kavishalom <prakashbabu95@gmail.com>
2021-11-09 00:04:47 -0800

    username functionality

--- src/org/test/Test.java
+++ src/org/test/Test.java
@@ -2,4 +2,14 @@ package org.test;
 
 public class Test {
 
+	private static void userName() {
+
+		System.out.println("username");
+	
+	}
+public static void main(String[] args) {
+	userName();
+	
 }
+}
+
